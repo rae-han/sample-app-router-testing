7b044c68023773fb547d9b7037850a97e627baa1
raehan <raehan900809@gmail.com>
2025-04-07 00:51:24 +0900

    feature: app router 기본 테스트 환경 구축

---
+++ src/app/first-test/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const FirstTestPage = () => {
+  return (
+    <div>
+      <h1>First Test Page</h1>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default FirstTestPage;
