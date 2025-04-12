e4631bed220d21fb7a24a550c29ca8c122b28d27
raehan <raehan900809@gmail.com>
2025-04-12 10:57:45 +0900

    feature: 테스트 코드 연습

--- src/app/shared/ui/text-field.test.tsx
+++ src/app/shared/ui/text-field.test.tsx
@@ -4,12 +4,25 @@ import { screen } from '@testing-library/react';
 
 describe('TextField', () => {
   it('텍스트를 입력하면, onChange prop으로 등록된 함수가 호출된다.', async () => {
-    const { user } = await render(<TextField />);
+    const spy = jest.fn();
+    const { user } = await render(<TextField onChange={spy} />);
 
-    const input = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+    const inputText = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
-    await user.type(input, 'test');
+    await user.type(inputText, 'test');
 
-    expect(input).toHaveValue('test');
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it('Enter 키를 누르면, onEnter prop으로 등록된 함수가 호출된다.', async () => {
+    const spy = jest.fn();
+    const { user } = await render(<TextField onEnter={spy} />);
+
+    const inputText = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(inputText, 'test');
+    await user.keyboard('{Enter}');
+
+    expect(spy).toHaveBeenCalledWith('test');
   });
 });
