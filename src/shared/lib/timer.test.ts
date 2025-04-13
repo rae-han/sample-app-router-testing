882c35ef377195b541323f1f47f8b667452a61ff
raehan <raehan900809@gmail.com>
2025-04-13 20:49:08 +0900

    feature: 디바운스 테스트

--- src/shared/lib/timer.test.ts
+++ src/shared/lib/timer.test.ts
@@ -20,4 +20,31 @@ describe('debounce', () => {
 
     expect(spy).toHaveBeenCalled();
   });
+
+  it('연이어 호출 시 특정 시간이 지난 함수만 호출된다.', () => {
+    const spy = jest.fn();
+
+    const debounceFn = debounce(spy, 400);
+
+    debounceFn();
+
+    jest.advanceTimersByTime(200);
+    debounceFn();
+
+    jest.advanceTimersByTime(400);
+    debounceFn();
+
+    jest.advanceTimersByTime(100);
+    debounceFn();
+
+    jest.advanceTimersByTime(200);
+    debounceFn();
+
+    jest.advanceTimersByTime(300);
+    debounceFn();
+
+    jest.advanceTimersByTime(400);
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
 });
