d5dacb71f9896024cba350c04cf730d3c5d0874d
raehan <raehan900809@gmail.com>
2025-04-16 08:42:21 +0900

    feature: todo 아이템 통합 테스트 샘플

--- src/app/todo/page.test.tsx
+++ src/app/todo/page.test.tsx
@@ -1,7 +1,7 @@
 import render from '@/shared/lib/render';
 import { mockUseTodoStore } from '@/shared/lib/test/zustand';
 import TodoPage from './page';
-import { screen } from '@testing-library/react';
+import { screen, within } from '@testing-library/react';
 
 beforeEach(() => {
   mockUseTodoStore({
@@ -14,8 +14,19 @@ beforeEach(() => {
 
 describe('Todo Page', () => {
   it('TODO 리스트에 포함된 아이템들의 이름, 완료 여부가 올바르게 표시되어야 한다.', () => {
-    // render(<TodoPage />);
+    render(<TodoPage />);
 
-    expect(true).toBe(true);
+    const todoItems = screen.getAllByRole('listitem');
+    expect(todoItems).toHaveLength(2);
+
+    const [firstTodoItem, secondTodoItem] = todoItems;
+
+    // 첫 번째 Todo 항목 검증
+    expect(within(firstTodoItem).getByText('test')).toBeInTheDocument();
+    expect(within(firstTodoItem).getByRole('checkbox')).not.toBeChecked();
+
+    // 두 번째 Todo 항목 검증
+    expect(within(secondTodoItem).getByText('test2')).toBeInTheDocument();
+    expect(within(secondTodoItem).getByRole('checkbox')).toBeChecked();
   });
 });
